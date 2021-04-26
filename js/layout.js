2684ddda426d1a34a6ae3dcd4e46738c32535995
IASlavnov <iaslavnov@gmail.com>
2021-04-26 16:30:23 +0300

    Добавляет кнопку ShowMore и отображение новостей

--- js/layout.js
+++ js/layout.js
@@ -23,33 +23,3 @@ viewList.addEventListener('click', (evt) => {
       }
   }
 });
-
-// 3 обработчика отдельно. Черновой вариант, если код выше норм, то удалю
-
-// const buttonLayoutList = document.querySelector('.view__button--list');
-// const buttonLayoutGridLarge = document.querySelector('.view__button--grid-large');
-// const buttonLayoutGridSmall = document.querySelector('.view__button--grid-small');
-
-// buttonLayoutList.addEventListener('click', () => {
-//   if (postsList.classList.contains('posts__list--grid-small') || postsList.classList.contains('posts__list--grid-large')) {
-//     postsList.classList.remove('posts__list--grid-small');
-//     postsList.classList.remove('posts__list--grid-large');
-//     postsList.classList.add('posts__list--list');
-//   }
-// });
-
-// buttonLayoutGridLarge.addEventListener('click', () => {
-//   if (postsList.classList.contains('posts__list--list') || postsList.classList.contains('posts__list--grid-small')) {
-//     postsList.classList.remove('posts__list--list');
-//     postsList.classList.remove('posts__list--grid-small');
-//     postsList.classList.add('posts__list--grid-large');
-//   }
-// });
-
-// buttonLayoutGridSmall.addEventListener('click', () => {
-//   if (postsList.classList.contains('posts__list--list') || postsList.classList.contains('posts__list--grid-large')) {
-//     postsList.classList.remove('posts__list--list');
-//     postsList.classList.remove('posts__list--grid-large');
-//     postsList.classList.add('posts__list--grid-small');
-//   }
-// });
--- js/main.js
+++ js/main.js
@@ -3,23 +3,44 @@ import './post-control.js';
 import {getData} from './news-api.js';
 import {renderElement} from './util.js';
 import Post from './post.js';
+import ShowMore from './show-more.js';
 
-const postsList = document.querySelector('.posts__list');
+const NEWS_PER_STEP = 10;
+const postsFeed = document.querySelector('.posts__feed');
+const postsList = postsFeed.querySelector('.posts__list');
 
-// const renderPost = (container, article) => {
-//   console.log(article);
-//   const post = new Post(article);
-//   post.getElement().querySelector('.post__watched').addEventListener('click', () => {
-//     console.log(post.getElement().querySelector('.post'));
-//     post.getElement().querySelector('.post').classList.toggle('post--read');
-//   });
+const renderNews = (news) => {
+  let renderedNewsCount = 0;
+  news
+    .slice(renderedNewsCount, NEWS_PER_STEP)
+    .forEach((elem) => {
+      renderElement(postsList, new Post(elem).getElement());
+    });
+  
+    renderedNewsCount += NEWS_PER_STEP;
 
-//   renderElement(container, post.getElement());
-// };
+  if (news.length > NEWS_PER_STEP) {
+    renderElement(postsFeed, new ShowMore().getElement());
+
+    const showMoreButton = postsFeed.querySelector('.posts__button');
+    showMoreButton.addEventListener('click', () => {
+      news
+        .slice(renderedNewsCount, renderedNewsCount + NEWS_PER_STEP)
+        .forEach((elem) => {
+          renderElement(postsList, new Post(elem).getElement());
+        });
+
+      renderedNewsCount += NEWS_PER_STEP;
+      if (renderedNewsCount >= news.length) {
+        showMoreButton.remove();
+      }
+    });
+  }
+};
 
 getData()
   .then((data) => {
-    console.log(data.articles[1]);
-    renderElement(postsList, new Post(data.articles[1]).getElement());
+    const {articles} = data;
+    renderNews(articles);
   })
   .catch((err) => console.log(err));
